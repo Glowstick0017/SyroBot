1ea19286403d6e8f70497ad2e5e97e6151ad4db3
Likhit Vyas Yarramsetti <73198499+likhity@users.noreply.github.com>
2021-12-07 19:18:58 -0700

    Added "can you give feedback" command
    Edited commands to start with "syro, " and added "can you give feedback" command

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ class MyClient(discord.Client):
 
         if message.content.upper() == 'SYRO':
             await message.channel.send('you can always do it yourself, right?')
-            
-        if message.content.lower() == 'can you help me with this?':
+        
+        if message.content.lower() == 'syro, can you help me with this?':
             await message.channel.send("I won't answer because I expect you to be reading the textbook")
+        
+        if message.content.lower() == 'syro, can you give me feedback so I can improve?':
+          await message.channel.send("Test cases did not work")
+
 
 client = MyClient()
-client.run('ENTER TOKEN HERE')
+client.run('OTE1MDk3ODc1ODIwNzE2MDky.YaWpTw.2230J-mRx8fNLaJbqsS-P-1TpuU')
